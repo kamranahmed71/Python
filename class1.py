e9ac0b9402e4e40cdaf0c1193bb69ee1565053a5
kamranahmed71 <kamranferoze71@gmail.com>
2023-10-17 12:31:00 +0500

    Class01

--- class1.py
+++ class1.py
@@ -1,2 +1,21 @@
 print("Welcome to Python Class")
 print("Kamran Ahmed")
+
+name : str = "Kamran Ahmed"
+print(name)   # print
+print(type(name))   # type
+print(id(name)) # physical address
+print([i for i in dir(name)])   # Methods and Attributes
+
+
+name : tuple[str,int,float,bool] = ("Daniyal", 5000, 15.653, True)
+print(name)
+print(type(name))
+print(id(name))
+print([i for i in dir(name)])
+
+name : any = "Daniyal"
+print(name)
+print(type(name))
+print(id(name))
+print([i for i in dir(name)])
